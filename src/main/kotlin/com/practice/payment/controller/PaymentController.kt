2beffd6c726307222f0efc36bf3d7a8441fee438
haebeen <haebing0309@gmail.com>
2024-04-21 16:37:34 +0900

    PaymentController, PaymentService 생성

---
+++ src/main/kotlin/com/practice/payment/controller/PaymentController.kt
@@ -0,0 +1,46 @@
+package com.practice.payment.controller
+
+import com.practice.payment.service.PaymentService
+import jakarta.validation.Valid
+import jakarta.validation.constraints.Min
+import jakarta.validation.constraints.NotBlank
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDateTime
+
+@RequestMapping("/api/v1")
+@RestController
+class PaymentController(
+    private val paymentService: PaymentService
+) {
+
+    @PostMapping("/pay")
+    fun pay(
+        @Valid @RequestBody payRequest: PayRequest
+    ): PayResponse {
+        return PayResponse("p1", 1000, "ts", LocalDateTime.now())
+    }
+}
+
+data class PayResponse(
+    val payUserId: String,
+    val amount: Long,
+    val transactionId: String,
+    val transactedAt: LocalDateTime
+)
+
+data class PayRequest(
+    @field:NotBlank
+    val payUserId: String,
+
+    @field:Min(100)
+    val amount: Long,
+
+    @field:NotBlank
+    val merchantTransactionId: String,
+
+    @field:NotBlank
+    val orderTitle: String,
+)
